f9874d62f58c895e5d1fd0c4d75e6298ceb3b29d
Chanapat R <pomi8891@gmail.com>
2020-05-02 19:52:15 +0700

    Commit Project

---
+++ javacode_project/Dockerfile
@@ -0,0 +1,4 @@
+FROM java:8  
+EXPOSE 80 
+ADD target/javacode_project.jar javacode_project.jar
+ENTRYPOINT ["java","-jar","/javacode_project.jar"]
---
+++ javacode_project/src/main/java/com/nairobin/javacode_project/App.java
@@ -0,0 +1,13 @@
+package com.nairobin.javacode_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
